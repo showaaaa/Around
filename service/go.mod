835c2f554423511abde556e3ab64968cdd90bee1
xw202 <xw202@duke.edu>
2021-11-03 22:41:39 +0800

    Authentication

--- service/go.mod
+++ service/go.mod
@@ -4,6 +4,10 @@ go 1.17
 
 require (
 	cloud.google.com/go/storage v1.18.2
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
+	github.com/gorilla/mux v1.8.0
 	github.com/pborman/uuid v1.2.1
 	gopkg.in/olivere/elastic.v3 v3.0.75
 )
--- service/main.go
+++ service/main.go
@@ -13,6 +13,10 @@ import (
 
 	"cloud.google.com/go/storage"
 
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	//"github.com/dgrijalva/jwt-go"
+	"github.com/form3tech-oss/jwt-go"
+	"github.com/gorilla/mux"
 	"github.com/pborman/uuid"
 	elastic "gopkg.in/olivere/elastic.v3"
 )
@@ -25,8 +29,11 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL      = "http://35.224.0.14:9200"
-	BUCKET_NAME = "post-images-330703"
+	ES_URL = "http://34.121.37.223:9200"
+	//34.121.37.223
+	//BUCKET_NAME = "post-images-330703"
+
+	BUCKET_NAME = "post-images-22"
 )
 
 type Location struct {
@@ -41,6 +48,8 @@ type Post struct {
 	Location Location `json:"location"`
 	Url      string   `json:"url"`
 }
+
+var mySigningKey = []byte("secret")
 
 func main() {
 	// Create a client
@@ -76,8 +85,25 @@ func main() {
 	}
 
 	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	http.HandleFunc("/search", handlerSearch)
+
+	r := mux.NewRouter()
+
+	var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
+	//http.HandleFunc("/post", handlerPost)
+	//http.HandleFunc("/search", handlerSearch)
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET")
+	r.Handle("/login", http.HandlerFunc(loginHandler)).Methods("POST")
+	r.Handle("/signup", http.HandlerFunc(signupHandler)).Methods("POST")
+
+	http.Handle("/", r)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
@@ -86,6 +112,10 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Contril-Allow-Headers", "Content-Type,Authorization")
+
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
 
 	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
 	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
@@ -97,7 +127,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 	p := &Post{
-		User:    "1111",
+		User:    username.(string),
 		Message: r.FormValue("message"),
 		Location: Location{
 			Lat: lat,
